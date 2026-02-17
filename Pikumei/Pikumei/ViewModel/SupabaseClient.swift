458e6d16d3f096df6922e93ebb31c0acac270f58
Erika Sakurai <erika03301109@gmail.com>
2026-02-17 18:30:27 +0900

    anon key設定

--- Pikumei/Pikumei/ViewModel/SupabaseClient.swift
+++ Pikumei/Pikumei/ViewModel/SupabaseClient.swift
@@ -10,8 +10,8 @@ import Supabase
 /// SPM で supabase-swift を追加後に使用可能になる
 enum SupabaseClientProvider {
 
-    private static let supabaseURL = URL(string: "https://osxadxaeisvecfhwwezc.supabase.co")!
-    private static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zeGFkeGFlaXN2ZWNmaHd3ZXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMzQ1MzEsImV4cCI6MjA4NjgxMDUzMX0.kw2xV8yQ6UuR1tRL137c4QK4rfRCCHedHRecDYBCGwA"
+    private static let supabaseURL = URL(string: "https://airfliszrpeclsenkaxp.supabase.co")!
+    private static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcmZsaXN6cnBlY2xzZW5rYXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNTU1NTMsImV4cCI6MjA4NjgzMTU1M30.ngibqzTDH-WnetZwZIlth8zk37fhC6vL1-L-s8z1vHs"
 
     static let shared = SupabaseClient(
         supabaseURL: supabaseURL,
