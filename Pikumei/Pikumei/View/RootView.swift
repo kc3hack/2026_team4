ca46f0603cbe71db183211c6dd1ed0be7b7fa48f
Ikkaku <hrkfji221@gmail.com>
2026-02-20 14:30:26 +0900

    Revert "Margeできなかったから一旦残しコメントにしといた"
    This reverts commit 66bb9c895ad4c46430e25869f1621cdf9deba491.

--- Pikumei/Pikumei/View/RootView.swift
+++ Pikumei/Pikumei/View/RootView.swift
@@ -7,9 +7,19 @@ import SwiftUI
 
 /// AppPhase に応じて表示する View を切り替えるルートビュー
 struct RootView: View {
+    @State private var phase: AppPhase = .launching
 
     var body: some View {
-        MainView()
+        ZStack {
+            switch phase {
+            case .launching:
+                LaunchingView(onFinish: {
+                    phase = .main
+                })
+            case .main:
+                MainView()
+            }
+        }
     }
 }
 
